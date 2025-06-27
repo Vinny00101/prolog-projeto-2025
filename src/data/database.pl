76f2bed9bc16f0d788981701bba05e4cd0fa1ffc
Vinny00101 <vini123gtbr@gmail.com>
2025-06-27 17:39:23 -0300

    feat: add movie database and  restructure rules

---
+++ src/data/database.pl
@@ -0,0 +1,44 @@
+:- set_prolog_flag(encoding, utf8).
+
+filme('Matrix', ficcao, 'Wachowski', 'Keanu Reeves, Laurence Fishburne', 1999, 141, frances, 'Estados Unidos', 9.4, 18).
+filme('Interestelar', ficcao, 'Christopher Nolan', 'Matthew McConaughey, Anne Hathaway', 2014, 171, japones, 'Estados Unidos', 9.2, 14).
+filme('A Origem', ficcao, 'Christopher Nolan', 'Leonardo DiCaprio, Joseph Gordon-Levitt', 2010, 148, frances, 'Estados Unidos', 6.2, 10).
+filme('Avatar', ficcao, 'James Cameron', 'Sam Worthington, Zoe Saldana', 2009, 172, portugues, 'Estados Unidos', 7.0, 12).
+filme('Duna', ficcao, 'Denis Villeneuve', 'Timothee Chalamet, Rebecca Ferguson', 2021, 173, coreano, 'Estados Unidos', 6.1, 10).
+filme('Blade Runner 2049', ficcao, 'Denis Villeneuve', 'Ryan Gosling, Harrison Ford', 2017, 174, coreano, 'Estados Unidos', 6.0, 18).
+filme('Ex_Machina', ficcao, 'Alex Garland', 'Alicia Vikander, Domhnall Gleeson', 2014, 152, japones, 'Reino Unido', 7.5, 14).
+filme('O Jogo da Imitacao', ficcao, 'Morten Tyldum', 'Benedict Cumberbatch, Keira Knightley', 2014, 87, frances, 'Reino Unido', 7.7, 14).
+filme('Distrito 9', ficcao, 'Neill Blomkamp', 'Sharlto Copley', 2009, 126, japones, 'Africa do Sul', 8.8, livre).
+filme('Gladiador', acao, 'Ridley Scott', 'Russell Crowe, Joaquin Phoenix', 2000, 179, frances, 'Estados Unidos', 6.7, 16).
+filme('Pantera Negra', acao, 'Ryan Coogler', 'Chadwick Boseman, Michael B. Jordan', 2018, 127, espanhol, 'Estados Unidos', 8.7, 18).
+filme('Mad Max: Estrada da Furia', acao, 'George Miller', 'Tom Hardy, Charlize Theron', 2015, 107, ingles, 'Australia', 6.7, 14).
+filme('Clube da Luta', drama, 'David Fincher', 'Edward Norton, Brad Pitt', 1999, 124, japones, 'Estados Unidos', 6.5, 16).
+filme('Whiplash', drama, 'Damien Chazelle', 'Miles Teller, J.K. Simmons', 2014, 173, portugues, 'Estados Unidos', 7.2, 18).
+filme('Coringa', drama, 'Todd Phillips', 'Joaquin Phoenix, Robert De Niro', 2019, 163, ingles, 'Estados Unidos', 9.0, 18).
+filme('Beleza Americana', drama, 'Sam Mendes', 'Kevin Spacey, Annette Bening', 1999, 116, espanhol, 'Estados Unidos', 9.1, 10).
+filme('O Quarto de Jack', drama, 'Lenny Abrahamson', 'Brie Larson, Jacob Tremblay', 2015, 167, portugues, 'Canada', 7.1, 14).
+filme('Requiem para um Sonho', drama, 'Darren Aronofsky', 'Ellen Burstyn, Jared Leto', 2000, 142, portugues, 'Estados Unidos', 9.1, 18).
+filme('Cisne Negro', suspense, 'Darren Aronofsky', 'Natalie Portman, Mila Kunis', 2010, 106, portugues, 'Estados Unidos', 7.2, 10).
+filme('Parasita', suspense, 'Bong Joon-ho', 'Song Kang-ho, Choi Woo-shik', 2019, 176, frances, 'Coreia do Sul', 8.8, 14).
+filme('Garota Exemplar', suspense, 'David Fincher', 'Ben Affleck, Rosamund Pike', 2014, 157, frances, 'Estados Unidos', 7.1, 10).
+filme('Os Outros', suspense, 'Alejandro Amenabar', 'Nicole Kidman', 2001, 149, portugues, 'Espanha', 7.7, 14).
+filme('Ilha do Medo', suspense, 'Martin Scorsese', 'Leonardo DiCaprio', 2010, 92, espanhol, 'Estados Unidos', 6.9, 18).
+filme('Seven - Os Sete Crimes Capitais', suspense, 'David Fincher', 'Brad Pitt, Morgan Freeman', 1995, 92, ingles, 'Estados Unidos', 9.0, 10).
+filme('A Viagem de Chihiro', animacao, 'Hayao Miyazaki', 'Rumi Hiiragi, Miyu Irino', 2001, 171, espanhol, 'Japao', 8.0, 14).
+filme('Toy Story', animacao, 'John Lasseter', 'Tom Hanks, Tim Allen', 1995, 107, portugues, 'Estados Unidos', 9.2, 16).
+filme('Up: Altas Aventuras', animacao, 'Pete Docter', 'Edward Asner, Jordan Nagai', 2009, 173, frances, 'Estados Unidos', 7.6, 18).
+filme('Wall-E', animacao, 'Andrew Stanton', 'Ben Burtt, Elissa Knight', 2008, 153, japones, 'Estados Unidos', 9.5, 16).
+filme('Divertida Mente', animacao, 'Pete Docter', 'Amy Poehler, Phyllis Smith', 2015, 144, japones, 'Estados Unidos', 6.9, livre).
+filme('Viva: A Vida e uma Festa', animacao, 'Lee Unkrich', 'Anthony Gonzalez, Gael Garcia Bernal', 2017, 124, portugues, 'Estados Unidos', 6.2, 12).
+filme('A Teoria de Tudo', biografia, 'James Marsh', 'Eddie Redmayne, Felicity Jones', 2014, 129, portugues, 'Reino Unido', 9.0, 18).
+filme('O Jogo da Imitacao', biografia, 'Morten Tyldum', 'Benedict Cumberbatch, Keira Knightley', 2014, 97, espanhol, 'Reino Unido', 9.2, livre).
+filme('Bohemian Rhapsody', biografia, 'Bryan Singer', 'Rami Malek, Lucy Boynton', 2018, 98, coreano, 'Reino Unido', 8.8, 10).
+filme('Rocketman', biografia, 'Dexter Fletcher', 'Taron Egerton, Jamie Bell', 2019, 142, espanhol, 'Estados Unidos', 8.9, 16).
+filme('Na Natureza Selvagem', biografia, 'Sean Penn', 'Emile Hirsch', 2007, 86, coreano, 'Estados Unidos', 7.0, 12).
+filme('Jobs', biografia, 'Joshua Michael Stern', 'Ashton Kutcher', 2013, 156, frances, 'Estados Unidos', 8.1, 18).
+filme('Os Infiltrados', policial, 'Martin Scorsese', 'Leonardo DiCaprio, Matt Damon', 2006, 87, espanhol, 'Estados Unidos', 7.6, 18).
+filme('Os Intocaveis', policial, 'Brian De Palma', 'Kevin Costner, Sean Connery', 1987, 136, japones, 'Brasil', 6.1, 14).
+filme('Tropa de Elite', policial, 'Jose Padilha', 'Wagner Moura', 2007, 178, portugues, 'Estados Unidos', 8.0, 14).
+filme('Zodiaco', policial, 'David Fincher', 'Jake Gyllenhaal, Mark Ruffalo', 2007, 171, japones, 'Brasil', 7.0, 10).
+filme('Cidade de Deus', policial, 'Fernando Meirelles', 'Alexandre Rodrigues, Leandro Firmino', 2002, 164, espanhol, 'Estados Unidos', 8.4, 10).
+filme('Scarface', policial, 'Brian De Palma', 'Al Pacino', 1983, 88, frances, 'Estados Unidos', 7.5, 14).
--- src/main.pl
+++ src/main.pl
@@ -1,17 +1,17 @@
-:- dynamic filme/6.
+:- ensure_loaded('data/database.pl').
 :- dynamic assistido/2.
 
 /*
-listing -> exibir o fato.
+listing -> exibir o fato.q
 assert -> adiciona ao fato dinamico.
 retract -> remove.
 */
-adicionar_filme(Nome, Genero, Atores, NotaIMDb, Classificacao, Pais) :-
-    assertz(filme(Nome, Genero, Atores, NotaIMDb, Classificacao, Pais)).
 
-% Remove um filme existente
-remover_filme(Nome) :-
-    retractall(filme(Nome, _, _, _, _, _)).
+/*Peso para cada criterio da recomendacao*/
+p_genero(2).
+p_ator_preferido(1).
+p_nota_imdb(1).
+p_pais(1).
 
 marcar_assistido(Usuario, Filme) :-
     assertz(assistido(Usuario, Filme)).
@@ -19,16 +19,34 @@ marcar_assistido(Usuario, Filme) :-
 desmarcar_assistido(Usuario, Filme) :-
     retractall(assistido(Usuario, Filme)).
 
+calcular_score(F1, F2, AtorPreferido, ScoreFinal).
+    filme(F1, G1, _, _, _, _, _, P1, IMDb1, _),
+    filme(F2, G2, _, Elenco2, _, _, _, P2, IMDb2, _),
+    /* continua o resto da regra*/
 
-explicacao_recomendacao(FilmeBase, FilmeComparado, Explicacao) :-
-    filme(FilmeBase, Genero, Atores, NotaIMDb, Classificacao, Pais),
-    filme(FilmeComparado, Genero2, Atores2, NotaIMDb2, Classificacao2, Pais2),
 
-    findall(Item,(
+explicacao_recomendacao(F1, F2, Explicacoes) :-
+    filme(F1, G1, _, _, _, _, _, P1, N1, _),
+    filme(F2, G2, _, _, _, _, _, P2, N2, _),
 
-        (Genero == Genero2 -> Item = 'Gênero semelhante'; fail);
-        /*Colocar uma recomendação por autor específico, fazer uma regra separada para isso e passa aqui na condição*/
-        (NotaIMDb >= NotaIMDb2 -> 'Nota IMDb igual ou maior'; fail);
-        (Pais == Pais2 -> 'Mesmo país de origem'; fail)
+    findall(Exp, (
+        (G1 == G2         -> Exp = 'Gênero semelhante'           ; fail);
+        (N2 >= N1         -> Exp = 'Nota IMDb igual ou maior'    ; fail);
+        (P1 == P2         -> Exp = 'Mesmo país de origem'        ; fail)
+    ), Explicacoes).
 
-    ), Explicacao).
+recomendacao_com_explicacao(FilmeBase, AtorPreferido, Usuario, ListaFinal) :-
+    findall(
+        Score-Titulo-Explicacoes,
+        (
+            filme(FilmeComparado, _, _, _, _, _, _, _, _, _),
+            FilmeComparado \= FilmeBase,
+            \+ assistido(Usuario, FilmeComparado),
+            calcular_score(FilmeBase, FilmeComparado, AtorPreferido, Score),
+            (Score =:= 4 ; Score =:= 3),
+            explicacao_recomendacao(FilmeBase, FilmeComparado, Explicacoes)
+
+        ),
+        ListaPontuada
+    ),
+    sort(0, @>=, ListaPontuada, ListaFinal).
